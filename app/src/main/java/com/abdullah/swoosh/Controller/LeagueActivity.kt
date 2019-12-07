436af567aa94e351ec8311eb5f6573144bffd9d3
Your Name <you@example.com>
2019-12-07 17:56:46 +0200

    Lesson 9

--- app/src/main/java/com/abdullah/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/abdullah/swoosh/Controller/LeagueActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.abdullah.swoosh.Model.Player
 import com.abdullah.swoosh.R
-import com.abdullah.swoosh.Utilities.EXTRA_LEAGUE
+import com.abdullah.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
-    var selectedLeague = ""
+    var player = Player("", "")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +19,9 @@ class LeagueActivity : BaseActivity() {
     }
 
     fun LeagueNextClicked(view: View) {
-        if(selectedLeague != "") {
+        if(player.league != "") {
             val skillActivity = Intent(this, SkillActivity::class.java)
-            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            skillActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(skillActivity)
         }else {
             Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
@@ -31,27 +32,27 @@ class LeagueActivity : BaseActivity() {
         coedLeagueBtn.isChecked = false
 
         if(mensLeagueBtn.isChecked)
-            selectedLeague = "mens"
+            player.league = "mens"
         else
-            selectedLeague = ""
+            player.league = ""
     }
     fun onWomensClicked(view: View) {
         mensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked = false
 
         if(womensLeagueBtn.isChecked)
-            selectedLeague = "womens"
+            player.league = "womens"
         else
-            selectedLeague = ""
+            player.league = ""
     }
     fun onCoedClicked(view: View) {
         mensLeagueBtn.isChecked = false
         womensLeagueBtn.isChecked = false
 
         if(coedLeagueBtn.isChecked)
-            selectedLeague = "co-ed"
+            player.league = "co-ed"
         else
-            selectedLeague = ""
+            player.league = ""
     }
 
 
--- app/src/main/java/com/abdullah/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/abdullah/swoosh/Controller/SkillActivity.kt
@@ -2,46 +2,44 @@ package com.abdullah.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.View
 import android.widget.Toast
+import com.abdullah.swoosh.Model.Player
 import com.abdullah.swoosh.R
-import com.abdullah.swoosh.Utilities.EXTRA_LEAGUE
-import com.abdullah.swoosh.Utilities.EXTRA_SKILL
+import com.abdullah.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : BaseActivity() {
 
-    var league = ""
-    var skill = ""
+    lateinit var player : Player
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
-        league = intent.getStringExtra(EXTRA_LEAGUE)
-        println(league)
+        player = intent.getParcelableExtra(EXTRA_PLAYER)
     }
 
     fun onBeginerClick(view: View) {
         ballerSkillBtn.isChecked = false
 
         if(beginerSkillBtn.isChecked)
-            skill = "begginer"
+            player.skill = "begginer"
         else
-            skill = ""
+            player.skill = ""
     }
     fun onBallerClick(view: View) {
         beginerSkillBtn.isChecked = false
 
         if(ballerSkillBtn.isChecked)
-            skill = "baller"
+            player.skill = "baller"
         else
-            skill = ""
+            player.skill = ""
     }
     fun onSkillFinishClicked(view: View) {
-        if(skill != "") {
+        if(player.skill != "") {
             val finishActivity = Intent(this, FinishActivity::class.java)
-            finishActivity.putExtra(EXTRA_LEAGUE, league)
-            finishActivity.putExtra(EXTRA_SKILL, skill)
+            finishActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(finishActivity)
         }else
             Toast.makeText(this, "Please select a skill level", Toast.LENGTH_SHORT).show()
