bc7a548297d98ffd994b8e3890f78f3e1da6e21d
Your Name <you@example.com>
2019-12-07 15:44:26 +0200

    Lesson 7

---
+++ app/src/main/java/com/abdullah/swoosh/Constants.kt
@@ -0,0 +1,3 @@
+package com.abdullah.swoosh
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/abdullah/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/abdullah/swoosh/WelcomeActivity.kt
@@ -1,15 +1,14 @@
 package com.abdullah.swoosh
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcome)
 
         getStartedBtn.setOnClickListener {
             val leagueIntent = Intent(this, LeagueActivity::class.java)
